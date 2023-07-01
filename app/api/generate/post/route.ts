479f9e422a1bb8dc8e01d96a4832104e6cda2bac
Rayan Kazi <rayankazi7515@gmail.com>
2023-07-01 11:35:23 -0700

    added try catch in generation api routes

--- app/api/generate/post/route.ts
+++ app/api/generate/post/route.ts
@@ -13,57 +13,74 @@ const postRouteSchema = z.object({
 })
 
 export async function POST(req: NextRequest) {
-  const body = await req.json()
-  const { name, description, tags, readTime } = postRouteSchema.parse(body)
-  const user = await getCurrentUser()
+  try {
+    const body = await req.json()
+    const { name, description, tags, readTime } = postRouteSchema.parse(body)
+    const user = await getCurrentUser()
 
-  if (!user)
+    if (!user)
+      return NextResponse.json(
+        { error: "Unauthorized to perform this action" },
+        { status: 401 }
+      )
+
+    const key = req.headers.get("Authorization")?.substring(7)
+    if (!key)
+      return NextResponse.json(
+        { error: "No API key", message: null },
+        { status: 400 }
+      )
+
+    // TODO: find other way to auth user for outside fetch
+    const remaining = Number(req.headers.get("x-remaining"))
+    await db.user.update({
+      where: { id: user?.id },
+      data: { remainingGens: remaining },
+    })
+
+    const { data } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0.8,
+      n: 1,
+      stream: false,
+      messages: [
+        {
+          role: "system",
+          content: `When responding, use everything provided in the prompt to formulate your response. Respond with a blog post written in markdown with proper headings and subheadings that can be read in about ${readTime} minutes. Don't make anything up while producing the response.`,
+        },
+        {
+          role: "user",
+          content: `Hi there, I want you to generate a blog post using the given fields. The title for the post will be ${name}. The description of the post will be ${description}. ${
+            tags && `The post should be related to the following tags: ${tags}`
+          } Make sure to include proper code snippets when available.`,
+        },
+      ],
+    })
+
+    console.log("DATA IS: ", data)
+    console.log(data.choices[0].message)
+
+    const message = data.choices[0].message?.content as string
+
+    await db.generation.create({
+      data: {
+        content: message,
+        type: "POST",
+        userId: user.id,
+      },
+    })
+
+    return NextResponse.json({ message, error: null }, { status: 200 })
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return NextResponse.json(
+        { error: error.issues, message: null },
+        { status: 422 }
+      )
+
     return NextResponse.json(
-      { error: "Unauthorized to perform this action" },
-      { status: 401 }
+      { error: "Something went wrong", message: null },
+      { status: 500 }
     )
-
-  const key = req.headers.get("Authorization")?.substring(7)
-  if (!key) return NextResponse.json({ error: "No API key" }, { status: 400 })
-
-  // TODO: find other way to auth user for outside fetch
-  const remaining = Number(req.headers.get("x-remaining"))
-  await db.user.update({
-    where: { id: user?.id },
-    data: { remainingGens: remaining },
-  })
-
-  const { data } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    temperature: 0.8,
-    n: 1,
-    stream: false,
-    messages: [
-      {
-        role: "system",
-        content: `When responding, use everything provided in the prompt to formulate your response. Respond with a blog post written in markdown with proper headings and subheadings that can be read in about ${readTime} minutes. Don't make anything up while producing the response.`,
-      },
-      {
-        role: "user",
-        content: `Hi there, I want you to generate a blog post using the given fields. The title for the post will be ${name}. The description of the post will be ${description}. ${
-          tags && `The post should be related to the following tags: ${tags}`
-        } Make sure to include proper code snippets when available.`,
-      },
-    ],
-  })
-
-  console.log("DATA IS: ", data)
-  console.log(data.choices[0].message)
-
-  const message = data.choices[0].message?.content as string
-
-  await db.generation.create({
-    data: {
-      content: message,
-      type: "POST",
-      userId: user.id,
-    },
-  })
-
-  return NextResponse.json({ message: data.choices[0].message?.content })
+  }
 }
--- app/api/generate/seo/route.ts
+++ app/api/generate/seo/route.ts
@@ -10,59 +10,76 @@ export const seoRouteSchema = z.object({
 })
 
 export async function POST(req: NextRequest) {
-  const body = await req.json()
-  const { post } = seoRouteSchema.parse(body)
-  const user = await getCurrentUser()
+  try {
+    const body = await req.json()
+    const { post } = seoRouteSchema.parse(body)
+    const user = await getCurrentUser()
 
-  if (!user)
+    if (!user)
+      return NextResponse.json(
+        { error: "Unauthorized to perform this action", message: null },
+        { status: 401 }
+      )
+
+    const key = req.headers.get("Authorization")?.substring(7)
+    if (!key)
+      return NextResponse.json(
+        { error: "No API key", message: null },
+        { status: 400 }
+      )
+
+    // TODO: get user auth some other way for outside fetching
+    const remaining = Number(req.headers.get("x-remaining"))
+    await db.user.update({
+      where: { id: user?.id },
+      data: { remainingGens: remaining },
+    })
+
+    const { data } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0.8,
+      n: 1,
+      stream: false,
+      messages: [
+        {
+          role: "system",
+          content:
+            "When responding, use everything provided in the prompt to formulate your response. Respond with a JSON object with the SEO optimized title as a string, the SEO optimized description as a string, and the SEO optimized tags as an array of strings.",
+        },
+        {
+          role: "user",
+          content: `Hi there, I would like an SEO optimized title, description, and some relavant SEO optimized tags for the following post that is written in markdown. Read the markdown post and use the post headings and content to formulate the response. The post is here: ${post}`,
+        },
+      ],
+    })
+
+    console.log("DATA IS: ", data)
+    console.log(data.choices[0].message)
+
+    const message = data.choices[0].message?.content as string
+
+    // const message = `
+    //   { "title": "How to Build a Modal Using Tailwind CSS and Headless UI | Tutorial", "description": "Learn how to build a custom modal using Tailwind CSS and Headless UI, with easy-to-follow code snippets and step-by-step instructions. Improve accessibility and user experience on your web applications today!", "tags": ["Tailwind CSS", "Headless UI", "Modal", "Web Development", "Tutorial"] }`
+
+    await db.generation.create({
+      data: {
+        content: message,
+        type: "SEO",
+        userId: user.id,
+      },
+    })
+
+    return NextResponse.json({ message, error: null }, { status: 200 })
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return NextResponse.json(
+        { error: error.issues, message: null },
+        { status: 422 }
+      )
+
     return NextResponse.json(
-      { error: "Unauthorized to perform this action" },
-      { status: 401 }
+      { error: "Something went wrong", message: null },
+      { status: 500 }
     )
-
-  const key = req.headers.get("Authorization")?.substring(7)
-  if (!key) return NextResponse.json({ error: "No API key" }, { status: 400 })
-
-  // TODO: get user auth some other way for outside fetching
-  const remaining = Number(req.headers.get("x-remaining"))
-  await db.user.update({
-    where: { id: user?.id },
-    data: { remainingGens: remaining },
-  })
-
-  const { data } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    temperature: 0.8,
-    n: 1,
-    stream: false,
-    messages: [
-      {
-        role: "system",
-        content:
-          "When responding, use everything provided in the prompt to formulate your response. Respond with a JSON object with the SEO optimized title as a string, the SEO optimized description as a string, and the SEO optimized tags as an array of strings.",
-      },
-      {
-        role: "user",
-        content: `Hi there, I would like an SEO optimized title, description, and some relavant SEO optimized tags for the following post that is written in markdown. Read the markdown post and use the post headings and content to formulate the response. The post is here: ${post}`,
-      },
-    ],
-  })
-
-  console.log("DATA IS: ", data)
-  console.log(data.choices[0].message)
-
-  const message = data.choices[0].message?.content as string
-
-  // const message = `
-  //   { "title": "How to Build a Modal Using Tailwind CSS and Headless UI | Tutorial", "description": "Learn how to build a custom modal using Tailwind CSS and Headless UI, with easy-to-follow code snippets and step-by-step instructions. Improve accessibility and user experience on your web applications today!", "tags": ["Tailwind CSS", "Headless UI", "Modal", "Web Development", "Tutorial"] }`
-
-  await db.generation.create({
-    data: {
-      content: message,
-      type: "SEO",
-      userId: user.id,
-    },
-  })
-
-  return NextResponse.json({ message })
+  }
 }
